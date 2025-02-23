9c3947c6c687ee5b4152115b930bc5707df8734f
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2025-02-22 16:24:22 -0800

    Adding Home Item

--- timeline_fe/my-app/src/App.tsx
+++ timeline_fe/my-app/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Pages/Home';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Home/>``
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
