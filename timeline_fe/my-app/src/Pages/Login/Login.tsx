186f8654d784155583972f181a19847edf24f4f8
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2025-02-23 16:04:25 -0800

    Customizing login page

--- timeline_fe/my-app/src/Pages/Login/Login.tsx
+++ timeline_fe/my-app/src/Pages/Login/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Login.css"
+import "./Login.css";
 export default function Login() {
   const [userName, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -7,21 +7,33 @@ export default function Login() {
   console.log(userName, password);
   return (
     <div className="flex-container">
-      <div className="flex-item">
-        <h2> Login </h2>
-        <input
-          type="text"
-          id="username"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        ></input>
-        <h2> Password </h2>
-        <input
-          type="text"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
+      <div id="login-side" className="flex-item1">
+        <h1> Welcome to TimeLines</h1>
+        <div className="input-placement">
+          <h2> Username </h2>
+          <input
+            type="text"
+            id="username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          ></input>
+        </div>
+        <div className="input-placement">
+          <h2> Password </h2>
+          <input
+            type="text"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+      </div>
+      <div className="flex-item2">
+        <img
+          src="/login_sample.jpg"
+          alt="login_screen_img"
+          id="login-screen-img"
+        />
       </div>
     </div>
   );
